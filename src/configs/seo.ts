6a5bcde457c8092adab4e2eefa5a5169e3acdd94
zomeru <zomerzxc.19@gmail.com>
2025-10-06 12:56:33 +0800

    Seo tweaks

--- src/configs/seo.ts
+++ src/configs/seo.ts
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next';
 
-export const DEFAULT_URL =
-  process.env.NODE_ENV === 'production' ? 'https://zomeru.com' : 'https://dev-zoms.vercel.app';
-export const SITE_URL = process.env.SITE_URL ?? DEFAULT_URL;
+export const SITE_URL = process.env.SITE_URL ?? '';
 export const title = 'Zomer Gregorio';
 export const description =
   'Hi 👋, I am Zomer, a Software Engineer based in the Philippines with a demonstrated history of working in the information technology and services industry. Skilled in React, Node, Typescript, and other web technologies with 2 years of professional experience in Full Stack Development.';
@@ -66,7 +64,6 @@ export const seo: Metadata = {
     `${title} | Front End Engineer`,
     `${title} | Full Stack Engineer`,
     `${title} | Web Developer`,
-    `${title} | React Developer`,
-    `${title} | Typescript Developer`
+    `${title} | React Developer`
   ]
 };
