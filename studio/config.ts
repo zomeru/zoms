520366419d999ac4845c65040bacef3e1319a38f
zomeru <zomerzxc.19@gmail.com>
2025-10-07 13:42:04 +0800

    Update sanity config

--- studio/config.ts
+++ studio/config.ts
@@ -1,18 +1,6 @@
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-const appId = process.env.NEXT_PUBLIC_SANITY_APP_ID;
-
-if (!projectId) {
-  throw new Error('Missing NEXT_PUBLIC_SANITY_PROJECT_ID environment variable');
-}
-
-if (!dataset) {
-  throw new Error('Missing NEXT_PUBLIC_SANITY_DATASET environment variable');
-}
-
-if (!appId) {
-  throw new Error('Missing NEXT_PUBLIC_SANITY_APP_ID environment variable');
-}
+const projectId = 'vap9ch2u';
+const dataset = 'production';
+const appId = 'rzockskbthk4eaw7hjx6t978';
 
 export const config = {
   dataset,
--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -2,14 +2,15 @@ import { visionTool } from '@sanity/vision';
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
 
+import { config } from './config';
 import { schemaTypes } from './schemas';
 
 export default defineConfig({
   name: 'default',
   title: 'Zoms Portfolio',
 
-  projectId: 'vap9ch2u',
-  dataset: 'production',
+  projectId: config.projectId,
+  dataset: config.dataset,
 
   plugins: [structureTool(), visionTool()],
 
