ff4b0fe30c25b47747f444ac6fa598094c385486
zomeru <zomerzxc.19@gmail.com>
2025-10-09 13:52:33 +0800

    Add dompurify

--- src/app/blog/[slug]/BlogContent.tsx
+++ src/app/blog/[slug]/BlogContent.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import DOMPurify from 'dompurify';
 
 interface BlogContentProps {
   body: string;
 }
 
 const BlogContent: React.FC<BlogContentProps> = ({ body }) => {
+  const sanitizedBody = DOMPurify.sanitize(body);
+
   return (
     <article
       className='prose prose-invert max-w-none markdown-content'
-      dangerouslySetInnerHTML={{ __html: body }}
+      dangerouslySetInnerHTML={{ __html: sanitizedBody }}
     />
   );
 };
